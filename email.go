888ccf027c9ad69747642a8e537fb0fc292b5662
igor-chornij <igor.chornij@gen.tech>
2021-11-19 09:58:54 +0200

    Implement email sending

---
+++ email.go
@@ -0,0 +1,76 @@
+package sendios
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/pkg/errors"
+	"net/http"
+	"os"
+)
+
+const (
+	CategorySystem  = "1"
+	CategoryTrigger = "2"
+)
+
+type (
+	EmailRequest struct {
+		TypeID   string                 `json:"type_id,omitempty"`
+		Category string                 `json:"category,omitempty"`
+		Data     Data                   `json:"data"`
+		Meta     map[string]interface{} `json:"meta,omitempty"`
+	}
+	Data struct {
+		User User `json:"user,omitempty"`
+	}
+	User struct {
+		Email      string `json:"email,omitempty"`
+		Name       string `json:"name,omitempty"`
+		Age        string `json:"age,omitempty"`
+		Gender     string `json:"gender,omitempty"`
+		Language   string `json:"language,omitempty"`
+		Country    string `json:"country,omitempty"`
+		PlatformID string `json:"platform_id,omitempty"`
+		Vip        string `json:"vip,omitempty"`
+		Photo      string `json:"photo,omitempty"`
+	}
+)
+
+type EmailResponse struct {
+	Meta `json:"_meta"`
+	Data struct {
+		Queued bool `json:"queued"`
+		RTime  int  `json:"r_time"`
+	} `json:"data"`
+}
+
+func (c *Client) SendEmail(r EmailRequest) (*EmailResponse, error) {
+	type req struct {
+		EmailRequest
+		ClientID  string
+		ProjectID int
+	}
+	data, err := json.Marshal(req{
+		EmailRequest: r,
+		ClientID:     os.Getenv("SENDIOS_CLIENT_ID"),
+		ProjectID:    c.Config.Project,
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "marshal payload")
+	}
+
+	_, body, err := c.makeRequest(http.MethodPost, "https://api.sendios.io/v1/push/system", bytes.NewReader(data))
+	if err != nil {
+		return nil, errors.Wrap(err, "send email")
+	}
+
+	var resp EmailResponse
+	if err := mapResponse(body, &resp); err != nil {
+		fmt.Println(string(body))
+
+		return nil, errors.Wrap(err, "map last payment response")
+	}
+
+	return &resp, nil
+}
---
+++ email_test.go
@@ -0,0 +1,38 @@
+package sendios
+
+import (
+	"github.com/joho/godotenv"
+	"github.com/stretchr/testify/require"
+	"os"
+	"strconv"
+	"testing"
+)
+
+func TestSendEmail(t *testing.T) {
+	require.NoError(t, godotenv.Load())
+
+	require.NotEmpty(t, os.Getenv("SENDIOS_PROJECT"))
+	require.NotEmpty(t, os.Getenv("SENDIOS_CLIENT_ID"))
+	require.NotEmpty(t, os.Getenv("SENDIOS_TOKEN"))
+	require.NotEmpty(t, os.Getenv("TEST_EMAIL"))
+
+	project, err := strconv.Atoi(os.Getenv("SENDIOS_PROJECT"))
+	require.NoError(t, err)
+	client, err := strconv.Atoi(os.Getenv("SENDIOS_CLIENT_ID"))
+	require.NoError(t, err)
+
+	c := New(project, client, os.Getenv("SENDIOS_TOKEN"))
+	c.DebugRequests = true
+
+	result, err := c.SendEmail(EmailRequest{
+		TypeID:   "1",
+		Category: CategorySystem,
+		Data: Data{
+			User: User{
+				Email: "igor.chornij@gen.tech",
+			},
+		},
+	})
+	require.NoError(t, err)
+	require.Equal(t, "SUCCESS", result.Meta.Status)
+}
